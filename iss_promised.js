fc751893ec5b52e9b8b00d58408bae5374bc6327
Laura Tannahill <lntannahill@gmail.com>
2020-11-06 14:04:58 +0000

    promise style

---
+++ iss_promised.js
@@ -0,0 +1,31 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ip-api.com/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const data = JSON.parse(body);
+  const coords = { latitude: data.lat, longitude: data.lon};
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
+  return request(url);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { passTimes } = JSON.parse(data);
+      return passTimes;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
+
+// module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
